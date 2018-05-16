be26856f16a9d462a92f4eb98727f962a56e0066
Rene Albrechtsen <r_albrechtsen@outlook.com>
2018-05-16 19:40:48 +0200

    Figuring out delete function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,24 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  weapon: string;
+  weapons: string[] = [];
+ 
+  
+  
   constructor(public navCtrl: NavController) {
-
+    
   }
-
+  addWeapon(){
+    this.weapons.push(this.weapon);
+    this.weapon = "";
+  }  
+  
+  delete(item){
+    var index = this.weapons.indexOf(item, 0);
+    if (index > -1) {
+      this.weapons.splice(index, 1);
+    }
+    console.log("this weapon was deleted", item);
+  }
 }
